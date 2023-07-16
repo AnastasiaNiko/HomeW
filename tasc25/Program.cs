844d6774d55af42178a8006f37c313aac443239c
Анастасия Николаева <nikolaeva@MacBook-Pro-Anastasia.local>
2023-07-16 16:08:41 +0300

    tasc25

---
+++ tasc25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B
+
+int InputNum (string message)
+{
+    Console.Write (message);
+    return Convert.ToInt32 (Console.ReadLine ());
+    }
+void Degree (int number, int degree)
+{
+    int result  = number;
+
+    for (int i = 2; i <= degree; i++)
+    {
+        result *= number;
+    }
+    Console.WriteLine ($" Результат: {result}");
+}
+
+int number = InputNum (" Введите число: ");
+
+int degree = InputNum (" В какую степень возвести: ");
+
+Degree(number, degree);

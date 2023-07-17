ba12d052ef0ee96409d2388bb747095d4beac26b
Анастасия Николаева <nikolaeva@MacBook-Pro-Anastasia.local>
2023-07-17 20:29:32 +0300

    tasc29

---
+++ tasc29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write(" Введите количество элементов  в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbres(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbres(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 9);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        // Console.Write($"[{String.Join(",", array.Length)}]"); 
+    }
+Console.Write($"[{String.Join(",", array)}]"); 
+}
